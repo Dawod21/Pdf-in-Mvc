8eb09e4ce1697f8db0786f093bb4674d6957d3d7
Dawod21 <dawoodmahar21@gmail.com>
2021-06-20 21:57:47 +0500

    generate pdf in asp.net mvc

---
+++ PDFMvc/Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using iTextSharp.text.html.simpleparser;
+using PDFMvc.Models;
+
+namespace PDFMvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            FBEntities obj = new FBEntities();
+
+            return View(from Employee in obj.Employees.Take(10)select Employee);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public FileResult Export(string GridHtml)
+        {
+            using(MemoryStream stream=new MemoryStream())
+            {
+                StringReader sr = new StringReader(GridHtml);
+                Document pdf = new Document(PageSize.A4, 10f, 10f, 100f, 0f);
+                PdfWriter writer = PdfWriter.GetInstance(pdf, stream);
+                pdf.Open();
+                XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdf, sr);
+                pdf.Close();
+                return File(stream.ToArray(), "application/pdf", "Grid.pdf");
+            }
+        }
+    }
+}
---
+++ PDFMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PDFMvc.Startup))]
+namespace PDFMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
